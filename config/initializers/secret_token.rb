96d7066120e89a3080a43ea0ae6208b11aaa5a89
jgrimm1158 <jonathangrimm1158@gmail.com>
2013-11-05 11:07:29 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trainstation::Application.config.secret_key_base = 'e7bc7e61a9b38499ff7dba29934b31408316f63f5482b8170e40d4f5265af78ca02062f590c75af1f57dcbba326bb8568a4ecacb052304d8fb60dd2298372d37'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Trainstation::Application.config.session_store :cookie_store, key: '_Trainstation_session'
